3c6b34790038f810a9363687771c313b97b9c080
AniahLiepard <aniah.garcia@techhigh.us>
2015-09-10 10:12:37 -0400

    While Loop Project
    https://www.thenewboston.com/videos.php?cat=31&amp;video=17978

---
+++ ProjectTen.java
@@ -0,0 +1,10 @@
+public class ProjectTen {
+	public static void main(String args[]){
+		int bugs = 0;
+		
+		while (bugs < 5){
+			System.out.println("Shhh..... don't tell Azlen......");
+		bugs++;
+		}
+	}
+}
